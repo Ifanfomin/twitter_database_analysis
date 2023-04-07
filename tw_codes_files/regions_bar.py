4255cc02a51169c33136d49d9693849de8101828
Ifanfomin <129762316+Ifanfomin@users.noreply.github.com>
2023-04-07 16:57:32 +0300

    Сделал столбчатую диаграмму по регионам

--- tw_codes_files/regions_bar.py
+++ tw_codes_files/regions_bar.py
@@ -1,28 +1,38 @@
 import sqlite3
 import matplotlib.pyplot as plt
 
-conn = sqlite3.connect(r'D:\project\dbase\tw_db_5mln\twitter_5mln.db')
+def sorting(regions_accounts):
+    sorted_values = sorted(regions_accounts.values())
+    sorted_dict = {}
+    for i in sorted_values:
+        for k in regions_accounts.keys():
+            if regions_accounts[k] == i:
+                sorted_dict[k] = regions_accounts[k]
+                break
+    return sorted_dict
+
+conn = sqlite3.connect(r'G:\project\dbase\tw_db_5mln\twitter_5mln.db')
 cur = conn.cursor()
 
-regions_accounts = {'DE': 0, 'RU': 0, 'USA': 0, 'LV': 0, 'UA': 0, 'Tokyo': 0, 'Japan': 0, 'China': 0}
-
+regions_accounts = {'DE': 0, 'RU': 0, 'USA': 0, 'UA': 0, 'Japan': 0, 'China': 0}
+summ_acc = 0
 for r in regions_accounts:
     query = 'SELECT count() FROM accounts WHERE location LIKE "%' + r + '%"'
     cur.execute(query)
     data = cur.fetchone()[0]
     regions_accounts[r] = data
+    summ_acc += data
 
-sorted_values = sorted(regions_accounts.values())
-sorted_regions_accounts ={}
-for i in sorted_values:
-    for k in regions_accounts.keys():
-        if regions_accounts[k] == i:
-            sorted_regions_accounts[k] = regions_accounts[k]
-            break
-index = sorted_regions_accounts.keys()
+for k, v in regions_accounts.items():
+    regions_accounts[k] = int(regions_accounts[k] / summ_acc * 100)
+
+sorted_regions_accounts = sorting(regions_accounts)
+
+index = ['Япония', 'Китай', 'Россия', 'Украина', 'Америка', 'Германия'] #  sorted_regions_accounts.keys()
 values = sorted_regions_accounts.values()
 
 plt.bar(index, values)
+plt.ylabel('создано аккаунтов в процентах')
 plt.show()
 
 print(sorted_regions_accounts)
