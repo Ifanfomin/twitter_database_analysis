e44778c3d270807a66a0d74ea5aad8b3d1a8e3ef
FxIxN <ifanfomin@yndex.ru>
2023-04-08 21:50:07 +0300

    Исправленный код регионов

--- tw_codes_files/regions/regions_bar.py
+++ tw_codes_files/regions/regions_bar.py
@@ -11,28 +11,24 @@ def sorting(regions_accounts):
                 break
     return sorted_dict
 
-conn = sqlite3.connect(r'D:\projects\dbase\twitterdb\tw_db_5mln')    # G:\project\dbase\tw_db_5mln\twitter_5mln.db
+conn = sqlite3.connect(r'D:\projects\dbase\twitterdb\tw_db_5mln\twitter_5mln.db')    # G:\project\dbase\tw_db_5mln\twitter_5mln.db
 cur = conn.cursor()
 
 regions_accounts = {'DE': 0, 'RU': 0, 'USA': 0, 'UA': 0, 'Japan': 0, 'China': 0}
-summ_acc = 0
+
 for r in regions_accounts:
     query = 'SELECT count() FROM accounts WHERE location LIKE "%' + r + '%"'
     cur.execute(query)
     data = cur.fetchone()[0]
-    regions_accounts[r] = data
-    summ_acc += data
-
-for k, v in regions_accounts.items():
-    regions_accounts[k] = int(regions_accounts[k] / summ_acc * 100)
+    regions_accounts[r] = data // 1000
 
 sorted_regions_accounts = sorting(regions_accounts)
 
-index = ['Япония', 'Китай', 'Россия', 'Украина', 'Америка', 'Германия'] #  sorted_regions_accounts.keys()
+index = ['Япония (Japan)', 'Китай (China)', 'Россия (RU)', 'Украина (UA)', 'Америка (USA)', 'Германия (DE)'] #  sorted_regions_accounts.keys()
 values = sorted_regions_accounts.values()
 
 plt.bar(index, values)
-plt.ylabel('создано аккаунтов в процентах')
+plt.ylabel('создано аккаунтов тысяч')
 plt.show()
 
 print(sorted_regions_accounts)
