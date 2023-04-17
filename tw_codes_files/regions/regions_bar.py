3705dc489ec134f4cec669195ca0bafd6433a2ce
Ifanfomin <129762316+Ifanfomin@users.noreply.github.com>
2023-04-17 17:16:38 +0300

    Код гистограммы по регионам

--- tw_codes_files/regions/regions_bar.py
+++ tw_codes_files/regions/regions_bar.py
@@ -6,7 +6,7 @@ def sorting(regions_accounts):
     sorted_dict = {}
     for i in sorted_values:
         for k in regions_accounts.keys():
-            if regions_accounts[k] == i:
+            if regions_accounts[k] == i and not(k in sorted_dict):
                 sorted_dict[k] = regions_accounts[k]
                 break
     return sorted_dict
@@ -14,7 +14,7 @@ def sorting(regions_accounts):
 conn = sqlite3.connect(r'D:\projects\dbase\twitterdb\tw_db_5mln\twitter_5mln.db')    # G:\project\dbase\tw_db_5mln\twitter_5mln.db
 cur = conn.cursor()
 
-regions_accounts = {'DE': 0, 'RU': 0, 'USA': 0, 'UA': 0, 'Japan': 0, 'China': 0}
+regions_accounts = {'DE': 0, 'RU': 0, 'UA': 0, 'Japan': 0, 'China': 0, 'FR': 0, 'UK': 0}
 
 for r in regions_accounts:
     query = 'SELECT count() FROM accounts WHERE location LIKE "%' + r + '%"'
@@ -24,7 +24,7 @@ for r in regions_accounts:
 
 sorted_regions_accounts = sorting(regions_accounts)
 
-index = ['Япония (Japan)', 'Китай (China)', 'Россия (RU)', 'Украина (UA)', 'Америка (USA)', 'Германия (DE)'] #  sorted_regions_accounts.keys()
+index = ['Япония (Japan)', 'Китай (China)', 'Соед. Кор. (UK)', 'Россия (RU)', 'Украина (UA)', 'Франция (FR)', 'Германия (DE)'] # sorted_regions_accounts.keys()
 values = sorted_regions_accounts.values()
 
 plt.bar(index, values)
